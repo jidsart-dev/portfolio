7bb520ce934ec1048da89d02009fdb095caea079
jidsart-dev <155943823+jidsart-dev@users.noreply.github.com>
2025-06-12 17:22:33 +0800

    Update ga-events.js

--- assets/js/ga-events.js
+++ assets/js/ga-events.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // Download CV Button
   const downloadBtn = document.getElementById("download-cv-btn");
-
   if (downloadBtn) {
     downloadBtn.addEventListener("click", function () {
       gtag('event', 'download_cv_click', {
@@ -10,28 +10,26 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     });
   }
+
+  // About Me Contact Button
+  const aboutcontactButton = document.getElementById('contact-button-about');
+  if (aboutcontactButton) {
+    aboutcontactButton.addEventListener('click', function () {
+      gtag('event', 'contact_click_about', {
+        event_category: 'Button',
+        event_label: 'Contact Me (About Section)'
+      });
+    });
+  }
+
+  // Navbar Contact Button
+  const navcontactButton = document.getElementById('contact-button-nav');
+  if (navcontactButton) {
+    navcontactButton.addEventListener('click', function () {
+      gtag('event', 'contact_click_nav', {
+        event_category: 'Button',
+        event_label: 'Contact Me (Navbar)'
+      });
+    });
+  }
 });
-
-
-// About Me Contact Button
-const aboutcontactButton = document.getElementById('contact-button-about');
-if (aboutcontactButton) {
-  aboutcontactButton.addEventListener('click', function() {
-    gtag('event', 'contact_click', {
-      'event_category': 'Button',
-      'event_label': 'Contact Me'
-    });
-  });
-}
-
-// Nav Contact Button
-const navcontactButton = document.getElementById('contact-button-nav');
-if (navcontactButton) {
-  navcontactButton.addEventListener('click', function() {
-    gtag('event', 'contact_click', {
-      'event_category': 'Button',
-      'event_label': 'Contact Me'
-    });
-  });
-}
-
